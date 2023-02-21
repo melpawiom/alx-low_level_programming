096598cc726265e9e64a77001d7ab936d67b0673
melpawiom <you@example.com>
2023-02-21 02:58:04 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program that print _putchar
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_hbt[9] = "_putchar";
+
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
