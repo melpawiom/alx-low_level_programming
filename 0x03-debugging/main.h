1c1afa8fe9909379b5d798436ec17e4e7b345c47
melpawiom <you@example.com>
2023-02-22 16:42:35 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* main_h */
